af3579fe7e98fc36043faba1acf91900e783d012
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2023-05-10 00:37:13 +0200

    seller per sales analyze

--- code/Analyze/SellerperMonthlySales.py
+++ code/Analyze/SellerperMonthlySales.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('Analyze/AMZ_Data_Clean.csv')
 
 # Création des catégories de ventes mensuelles
-bins = [0, 10, 20, 30, 310]
-labels = ['1-10', '11-20', '21-30', '31-310']
+bins = [0, 10, 20, 50, 100, 500, 1000, 10000, 20000]
+labels = ['1-10', '11-20', '21-50', '51-100', '101-500', '501-1000', '1001-10000', '10001-20000']
 df['Monthly Sales Category'] = pd.cut(df['Monthly Sales'], bins=bins, labels=labels)
 
 # Calcul du nombre de vendeurs par catégorie de ventes mensuelles
--- code/Analyze/analyze.py
+++ code/Analyze/analyze.py
@@ -3,6 +3,18 @@ import pandas as pd
 
 df = pd.read_csv('Analyze/AMZ_Data_Clean.csv')
 
-seller = df['Reviews Rating'].max()
+note = [1, 2, 3, 4, 5]
+labels = ['1-2', '2-3', '3-4', '4-5']
 
-print(seller)
+df['Review'] = pd.cut(df['Reviews Rating'], bins=note, labels=labels)
+group = df.groupby ('Review') ['Monthly Sales'].sum()
+
+group.plot(kind='bar')
+
+# Configuration des labels et des titres
+plt.xlabel('Catégories de ventes mensuelles')
+plt.ylabel('Nombre de vendeurs')
+plt.title('Nombre de vendeurs par catégorie de ventes mensuelles')
+
+# Affichage du graphique
+plt.show()
