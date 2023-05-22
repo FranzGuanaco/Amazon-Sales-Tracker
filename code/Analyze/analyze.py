849eefd4c553129f6ebdb5d88f76591821540509
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2023-05-23 01:19:48 +0200

    selenium export data step

--- code/Analyze/analyze.py
+++ code/Analyze/analyze.py
@@ -4,6 +4,9 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 print(selenium.__version__)
 
@@ -33,6 +36,8 @@ username_selector = '#loginform-email'
 password_selector = '#loginform-password'
 login_button_selector = '.btn.btn-secondary.btn-block'
 
+
+
 # Localisez les champs de saisie des identifiants et le bouton de connexion
 username_field = driver.find_element("css selector", "#loginform-email")
 password_field = driver.find_element("css selector", password_selector)
@@ -41,18 +46,19 @@ login_button = driver.find_element("css selector", login_button_selector)
 # Remplissez les champs de saisie avec vos identifiants
 username_field.send_keys(username)
 password_field.send_keys(password)
-
+sleep(20)
 # Cliquez sur le bouton de connexion
 login_button.click()
 
-# Attendez que la page se charge complètement (ajustez le temps d'attente si nécessaire)
 sleep(5)
 
 # Localisez le bouton de téléchargement et cliquez dessus
 # Assurez-vous que le sélecteur CSS est correct
-download_button_selector = '.sc-llYSUQ.bhKSKZ'  # Ici, il manque un point entre les deux classes
-download_button = driver.find_element_by_css_selector(download_button_selector)
+download_button_selector = '.sc-ikJyIC.kmfDQj'  # Sélecteur CSS du bouton de téléchargement
+download_button = driver.find_element("css selector", download_button_selector)
 download_button.click()
+
+
 
 # Attendez que le fichier soit téléchargé
 sleep(10)
@@ -60,4 +66,3 @@ sleep(10)
 # Fermez le navigateur
 driver.quit()
 
-# Maintenant, vous pouvez lire le fichier Excel avec pandas
