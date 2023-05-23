f48c4a894aeecd78f372595658c94b732171a670
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2023-05-24 00:35:03 +0200

    scraping test done

--- code/Analyze/analyze.py
+++ code/Analyze/analyze.py
@@ -1,5 +1,7 @@
 import os
 import selenium
+import subprocess
+import sys
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
@@ -54,14 +56,56 @@ sleep(5)
 
 # Localisez le bouton de téléchargement et cliquez dessus
 # Assurez-vous que le sélecteur CSS est correct
+# Localisez le bouton de téléchargement
 download_button_selector = '.sc-ikJyIC.kmfDQj'  # Sélecteur CSS du bouton de téléchargement
 download_button = driver.find_element("css selector", download_button_selector)
-download_button.click()
 
+# Faites défiler l'élément dans la vue
+driver.execute_script("arguments[0].scrollIntoView(true);", download_button)
 
+# Cliquez sur le bouton de téléchargement à l'aide de JavaScript
+driver.execute_script("arguments[0].click();", download_button)
 
 # Attendez que le fichier soit téléchargé
 sleep(10)
+
+# Localisez le bouton déroulant
+dropdown_button_selector = '.sc-ikJyIC.sc-hrjYtz.jKrrPL.dXeRAJ.sc-gHERNO.dDsDpY'
+dropdown_button = driver.find_element("css selector", dropdown_button_selector)
+
+# Cliquez sur le bouton déroulant
+dropdown_button.click()
+
+sleep(1)
+
+# Localisez le bouton de téléchargement du document
+download_button_selector = '.sc-caiLqq.haRiMa'
+download_button = driver.find_element("css selector", download_button_selector)
+
+# Cliquez sur le bouton de téléchargement du document
+download_button.click()
+
+sleep(3)
+
+download_dir = os.path.join(os.getcwd(), 'downloads')
+
+# Liste tous les fichiers du répertoire de téléchargement
+files = os.listdir(download_dir)
+
+# Parcourez les fichiers et ouvrez le fichier souhaité
+for file in files:
+    if file.endswith('.csv'):  # Remplacez '.csv' par l'extension de votre fichier
+        file_path = os.path.join(download_dir, file)
+        # Ouvre le fichier avec l'application par défaut du système
+        if sys.platform.startswith('darwin'):  # Pour macOS
+            subprocess.call(['open', file_path])
+        elif sys.platform.startswith('win32'):  # Pour Windows
+            subprocess.call(['start', file_path], shell=True)
+        elif sys.platform.startswith('linux'):  # Pour Linux
+            subprocess.call(['xdg-open', file_path])
+        break
+
+sleep(12)
 
 # Fermez le navigateur
 driver.quit()
